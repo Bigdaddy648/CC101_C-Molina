317f5de07ffefe217af0921c34a700d74a884592
Bigdaddy648 <mizuuchiha123456@gmail.com>
2024-10-04 09:34:30 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "School: \t\t\t | Our Lady of Lourdes College Foundation" << endl;
+	cout << "Department: \t\t\t | Computer Studies" << endl;
+	cout << "Course & Year Level: \t\t\t | BS in Information Technology" << endl;
+	cout << "Full name \t\t\t | Clarke Jay R. Molina" << endl;
+	cout << "Address \t\t\t | P-5 Bagong Silang 2, Labo, Camarines Norte" << endl;
+	
+	return 0;
+	
+}
