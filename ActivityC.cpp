dacccd78af376fd1a617c92f4e8bff188fa3303a
CJ Molina6969 <mizuuchiha123456@gmail.com>
2024-12-01 00:00:13 +0900

    Add files via upload

---
+++ ActivityC.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main ()
+
+{
+	int n = 8;
+	for (int i = 8; i >=1; i--) {
+		for (int k = n-i; k > 0; k--)
+			cout <<"  ";
+		for (int j = i; j > 0; j--) {
+			cout << "# ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
